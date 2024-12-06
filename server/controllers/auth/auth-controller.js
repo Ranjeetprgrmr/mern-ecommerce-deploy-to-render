5c9da199ca519f48fb605b623df5949bb04a3b4b
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-06 19:27:58 +0545

    changes cookie config

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -61,7 +61,7 @@ const loginUser = async(req, res) => {
             {expiresIn : '60mins'}
         );
 
-        res.cookie('token', token, {httpOnly: true, secure: false}).json({
+        res.cookie('token', token, {httpOnly: true, secure: true}).json({
             success : true,
             message : 'Login successfull',
             user: {
